2ec8bf2ec83a96938d8241fbcc2e5fb3703dccb7
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 14:49:04 +0000

    implemented twitter feature

--- config/application.rb
+++ config/application.rb
@@ -16,14 +16,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Devcampportfolio
+module DevcampPortfolio
   class Application < Rails::Application
-   config.generators do |g|
-        g.orm             :active_record
-        g.template_engine :erb
-        g.test_framework  :test_unit, fixture: false
-        g.stylesheets     false
-        g.javascripts     true 
-    end
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
