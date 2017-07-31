41356a1c7de429d5c2cd460c940e96aabe90dd9a
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 17:29:38 +0000

    built out topic views

---
+++ test/controllers/topic_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TopicControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get topic_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get topic_show_url
+    assert_response :success
+  end
+
+end
